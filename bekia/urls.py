ccdc81bc634ca41dc877d50156cc79d1abaf4b7b
zeinabmohammed <zobalhlooba@gmail.com>
2019-06-17 20:42:57 +0200

    add homepage design and user update view

--- bekia/urls.py
+++ bekia/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, profile
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('profile/', profile , name='profile'),
 ]
--- bekia/views.py
+++ bekia/views.py
@@ -1,4 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from .forms import UserUpdateForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
 	return render(request, 'index.html',{})
+
+
+
+
+@login_required
+def profile(request):
+
+	user = User.objects.get(id=request.user.id)
+	if request.method =='POST':
+		u_form = UserUpdateForm(request.POST,instance=request.user)
+		
+		if u_form.is_valid() :
+			u_form.save()
+		
+		messages.success(request, f'Yout Account successfully Updated')
+		return redirect('/')
+	else:
+		u_form = UserUpdateForm(instance=request.user)
+		
+
+
+	context={
+			'user_update': u_form,
+			
+			}
+	return render(request, 'users/profile.html', context)
